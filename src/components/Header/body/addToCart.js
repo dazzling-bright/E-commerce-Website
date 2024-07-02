759e87e53169b940f16375984e48370a83acefa1
Bright Udoette <brightudoette@gmail.com>
2024-07-02 14:23:37 +0100

    Implement hover effect on the AddToCart button using clippath

--- src/components/Header/body/addToCart.js
+++ src/components/Header/body/addToCart.js
@@ -1,11 +1,13 @@
 import CartIcon from "../../../images/header/icon-cart.svg";
+import styles from "./css/addToCart.module.css";
 
 function AddToCart() {
   return (
-    /** Add to Cart Button */
-    <button className="flex items-center bg-orange rounded-2xl p-4 text-very-dark-blue justify-center">
-      <img src={CartIcon} alt="Cart Icon" />
-      <span className="ml-3 font-bold">Add to cart</span>
+    <button
+      className={`${styles.addToCart} group relative flex items-center bg-orange rounded-2xl p-4 text-very-dark-blue justify-center`}
+    >
+      <img src={CartIcon} alt="Cart Icon" className="relative z-10" />
+      <span className="ml-3 font-bold relative z-10">Add to cart</span>
     </button>
   );
 }
