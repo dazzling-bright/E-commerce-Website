061a5a894bfd1abdc04ea65d752744170e36d266
Bright Udoette <brightudoette@gmail.com>
2024-07-03 02:13:31 +0100

    Improve the navListItems component for proper display on screen resizing

--- src/components/Header/NavContent.js
+++ src/components/Header/NavContent.js
@@ -4,10 +4,11 @@ const NavContent = ({ isOpen, handleMenuDisplay }) => {
   const listItems = ["Collections", "Men", "Women", "About", "Contact"];
 
   return (
-    <aside
-      className={`absolute z-50 w-[50vh] p-12 bg-white left-0 top-0 h-screen shadow-lg transform transition-transform duration-300 md:w-auto md:p-0 md:relative md:block md:items-center md:h-auto md:translate-x-0 md:bg-transparent md:shadow-none ${
-        isOpen ? "translate-x-0" : "-translate-x-full"
-      } ${isOpen ? "" : "hidden"}
+    <nav
+      className={`absolute z-50 w-[60vh] p-12 opacity-100 bg-white left-0 top-0 h-screen shadow-lg transform transition-transform duration-300 md:w-auto md:p-0 md:relative md:block md:items-center md:h-auto md:translate-x-0 md:bg-transparent md:shadow-none ${
+        isOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
+      }  ${isOpen ? "" : "w-0 h-0 overflow-hidden"}
+     
       }`}
     >
       <img
@@ -20,13 +21,13 @@ const NavContent = ({ isOpen, handleMenuDisplay }) => {
         {listItems.map((item, index) => (
           <li
             key={index}
-            className="md:mt-2 mt-4 font-bold  md:mx-1 lg:mx-3 py-4 md:py-8 text-xl cursor-pointer hover:opacity-80 border-b-4 border-transparent hover:border-orange transition-all duration-300"
+            className="md:mt-2 mt-4 font-bold sm:hover:ml-2 md:hover:ml-4  md:mx-1 lg:mx-3 py-4 md:py-8 text-xl cursor-pointer hover:opacity-80 border-b-4 border-transparent hover:border-orange transition-all duration-300"
           >
             {item}
           </li>
         ))}
       </ul>
-    </aside>
+    </nav>
   );
 };
 
